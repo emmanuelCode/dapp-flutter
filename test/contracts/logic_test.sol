06aaebd18d21b5374d5891f786205f19f492dc7a
emmanuelCode <e96e6@hotmail.com>
2024-01-28 17:11:15 -0500

    first commit

---
+++ test/contracts/logic_test.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+import "remix_tests.sol"; // this import is automatically injected by Remix.
+import "remix_accounts.sol";
+import "hardhat/console.sol";
+
+contract LogicTest {
+ 
+}
