9cefe49c25bcb3500f49459e8d89545cf0fe2185
emmanuelCode <e96e6@hotmail.com>
2024-02-23 00:17:38 -0500

    prototype: testing and added readme notes

--- README.md
+++ README.md
@@ -4,13 +4,64 @@ A new Flutter project.
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+To run this project you'll need some requirement:
 
-A few resources to get you started if this is your first Flutter project:
+- nodejs with npm installed
+- installing hardhat with  `npm install --save-dev hardhat` 
+- make sure that the hardhat toolbox is also installed `npm install --save-dev @nomicfoundation/hardhat-toolbox`
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+- make sure to configure `hardhat.config.js`:
+```javascript
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");// added this
+module.exports = {
+  solidity: "0.8.24",
+};
+```
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+- make sure that the `deploy.js` script is as follows:
+```javascript
+async function main() {
+
+const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+  
+  //deploy storage contract (first deployed contract)
+  const storage = await ethers.deployContract("Storage"); // class must match names when deploying
+  console.log('Deploying storage ...');
+  
+  let storageAddress = await storage.getAddress();
+
+  console.log('storage deployed to:', storageAddress);
+  
+
+  //deploy bounty contract
+  const logic = await ethers.deployContract("Logic");
+  console.log('Deploying logic ...');
+  
+  let logicAddress = await logic.getAddress();
+
+  console.log('logic deployed to:', logicAddress);
+
+
+
+  console.log( "storage: " + storageAddress );
+  console.log( "logic: " + logicAddress ); 
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+```
+
+- hardhat require you to have `contracts` folder available in the root project so make sure that .sol files are in the folder.
+
+- to launch an ethereum test chain open a terminal in the same directory and run `npx hardhat node`
+
+- to connect the flutter web app to use the test chain open another terminal and `flutter run web -d chrome` or `flutter run -d web-server`
+
+- to deploy a smart contract run it script with `npx hardhat run --network localhost scripts/deploy.js` you may need to compile before with `npx hardhat compile`
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart';
+import 'package:web3dart/crypto.dart';
+import 'package:web3dart/web3dart.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,40 +19,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Crypto Demo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +36,133 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Future<String> _loadContractAbi() async {
+    final contractCode =
+        await rootBundle.loadString('contracts/artifacts/Logic.json');
+    final abi = jsonDecode(contractCode)['abi'];
+    debugPrint(abi.toString());
+
+    return jsonEncode(abi);
+  }
+
+  Future<Uint8List> _loadContractData() async {
+    final contractCode =
+        await rootBundle.loadString('contracts/artifacts/Logic.json');
+    final bytecode = jsonDecode(contractCode)['data']['bytecode']['object'];
+
+    //final data = jsonEncode(bytecode);
+    debugPrint(bytecode);
+
+    return hexToBytes(bytecode);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var apiUrl = "http://localhost:8545"; //Replace with your API
+
+    final ethClient = Web3Client(apiUrl, Client());
+
+    EthPrivateKey credentials = EthPrivateKey.fromHex(
+        '0xc526ee95bf44d8fc405a158bb884d9d1238d99f0612e9f33d006bb0789009aaa');
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Web3Dart Test',
+               style: Theme.of(context).textTheme.headlineMedium
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                callAddFunction(contractAdressTemp!, credentials, ethClient);
+              },
+              child: const Text('Transac'),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () async {
+          debugPrint('hey');
+
+          deployContractTransact(ethClient, credentials);
+        },
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
+  }
+
+  EthereumAddress? contractAdressTemp;
+
+  // deploy smart contract using web3dart methods
+  void deployContractTransact(
+      Web3Client client, Credentials credentials) async {
+
+    // load contract data from Remix json     
+    final transaction = Transaction(data: await _loadContractData());
+
+  
+    final transactionHash =
+        await client.sendTransaction(credentials, transaction, chainId: 31337);
+
+      // output transactionReceipt
+    final transactionReceipt =
+        await client.getTransactionReceipt(transactionHash);
+    debugPrint('receipt: $transactionReceipt');
+
+    // output deployed contract address
+    debugPrint('contract address: ${transactionReceipt?.contractAddress}');
+
+    contractAdressTemp = transactionReceipt?.contractAddress;
+  }
+
+  // calls add function from Logic.sol file
+  void callAddFunction(EthereumAddress contractAddress, Credentials credentials,
+      Web3Client client) async {
+
+    // load contract from abi    
+    DeployedContract contract = DeployedContract(
+        ContractAbi.fromJson(await _loadContractAbi(), 'logic'),
+        contractAddress);
+
+    final transaction = Transaction.callContract(
+      // don't need to specify address will take from credential
+      //from: EthereumAddress.fromHex('0xdf3e18d64bc6a983f673ab319ccae4f1a57c7097'),
+      contract: contract,
+      function: contract.function('add'),
+      parameters: [],
+      value: EtherAmount.fromBigInt(EtherUnit.ether, BigInt.one),
+      // will let the network estimate the gas price
+      //gasPrice: EtherAmount.inWei(BigInt.from(1000000000)),
+      // I won't set a limit for gas price
+      //maxGas: 300000,
+    );
+
+    final transactionHash = await client.sendTransaction(
+        credentials, transaction,
+        chainId: 31337 //hardhat chain id
+        );
+
+    // output transactionHash
+    debugPrint('transactionHash: $transactionHash');
+
+    // output transaction receipt
+    final receipt = await client.getTransactionReceipt(transactionHash);
+    debugPrint('receipt: $receipt');
+
+    // output contract balance
+    final logicContractBalance = await client.getBalance(contractAddress);
+    debugPrint('logicContractBalance: $logicContractBalance');
+
+    // output account balance
+    final accountBalance = await client.getBalance(credentials.address);
+    debugPrint('accountBalance: $accountBalance');
   }
 }
