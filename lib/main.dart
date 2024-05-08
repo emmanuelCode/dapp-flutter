09dd09a30f825e89e5a35589e6e3c51ac5400ce6
emmanuelCode <e96e6@hotmail.com>
2024-05-08 00:41:57 -0400

    refactor: separate model from widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,7 @@
-import 'dart:convert';
-
 import 'package:cryptofont/cryptofont.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_dapp_sample/models/wallet_transactions.dart';
 import 'package:web3modal_flutter/web3modal_flutter.dart';
-import 'package:http/http.dart' as http;
-
-const projectID = String.fromEnvironment('PROJECT_ID');
-
-const _chainId = '31337'; //hardhat default chainId
-
-final _hardHatChain = W3MChainInfo(
-  chainName: 'Hardhat',
-  namespace: 'eip155:$_chainId',
-  chainId: _chainId,
-  tokenName: 'ETH',
-  rpcUrl: 'http://localhost:8545',
-);
 
 final _themeData = ThemeData(
   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -57,164 +41,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late final W3MService _w3mService;
-  final client = Web3Client(_hardHatChain.rpcUrl, http.Client());
-
-  var _contractAbi = '';
-  var _contractData = Uint8List(0);
-  var _contractAddress = '';
+  late final WalletTransactions walletTransactions;
 
   var _etherUnitValue = EtherUnit.values.first;
   final _textEditAmount = TextEditingController();
-  var _currentContractBalance = BigInt.zero;
+  var _currentContractBalance = 0.0;
 
   @override
   initState() {
     super.initState();
-    _initWalletConnect();
-    _loadContract();
-  }
-
-  void _initWalletConnect() async {
-    _w3mService = W3MService(
-      projectId: projectID,
-      //logLevel: LogLevel.debug,
-      metadata: const PairingMetadata(
-        name: 'Web3Modal Flutter Example',
-        description: 'Web3Modal Flutter Example',
-        url: 'https://www.walletconnect.com/',
-        icons: ['https://walletconnect.com/walletconnect-logo.png'],
-        redirect: Redirect(
-          native: 'flutterdapp://',
-          universal: 'https://www.walletconnect.com',
-        ),
-      ),
-    );
-
-    // add hardhat chain to presets
-    W3MChainPresets.chains
-        .putIfAbsent(_hardHatChain.chainId, () => _hardHatChain);
-    await _w3mService.init();
-  }
-
-  // load contract from Remix json
-  void _loadContract() async {
-    final contractCode =
-        await rootBundle.loadString('contracts/artifacts/Logic.json');
-    final abi = jsonDecode(contractCode)['abi'];
-    final bytecode = jsonDecode(contractCode)['data']['bytecode']['object'];
-
-    debugPrint(bytecode);
-    debugPrint(abi.toString());
-
-    _contractAbi = jsonEncode(abi);
-    _contractData = hexToBytes(bytecode);
-  }
-
-  // to learn more about rpc methods:
-  // https://ethereum.org/en/developers/docs/apis/json-rpc/
-  void _deployContract() async {
-    final transaction = Transaction(
-      // you need to define the from when doing transaction to push contract code
-      from: EthereumAddress.fromHex(_w3mService.session!.address!),
-      data: _contractData,
-    );
-
-    debugPrint(_w3mService.selectedChain!.namespace);
-    debugPrint(_w3mService.session!.topic!);
-    debugPrint(_w3mService.session!.address);
-
-    // always launch the wallet before making a Transaction with Web3Modal
-    _w3mService.launchConnectedWallet();
-
-    final String transactionHash = await _w3mService.request(
-      topic: _w3mService.session!.topic!,
-      // we need the namespace format here
-      chainId: _w3mService.selectedChain!.namespace,
-      request: SessionRequestParams(
-        // w3model has string constant to help with request
-        method: MethodsConstants.ethSendTransaction,
-        params: [transaction.toJson()],
-      ),
-    );
-
-    debugPrint('hash: $transactionHash');
-
-    //web3modal doesn't support `eth_getTransactionHash` with _w3mService.request(). However, you have the Web3dart
-    // bundle in Web3Modal so you can use it to get the package like so.
-    // you can list what available with _w3mService.session!.getApprovedMethods()
-    final transactionReceipt =
-        await client.getTransactionReceipt(transactionHash);
-    final transactionInformation =
-        await client.getTransactionByHash(transactionHash);
-    final networkId = await client.getNetworkId();
-
-    debugPrint('transactionReceipt: $transactionReceipt');
-    debugPrint('transactionInformation: $transactionInformation');
-    debugPrint('network: $networkId');
-
-    debugPrint(
-        'calulate cost gasUnits X gasPriceForOneUnit ${transactionInformation!.gas * transactionInformation.gasPrice.getInWei.toInt()}');
-    debugPrint('transaction cost: ${transactionReceipt!.gasUsed}');
-
-    _contractAddress = transactionReceipt.contractAddress!.hex;
+    walletTransactions = WalletTransactions();
   }
 
   void _addEtherRequest() async {
-    _w3mService.launchConnectedWallet();
-    final String transactionHash = await _w3mService.requestWriteContract(
-      rpcUrl: _w3mService.selectedChain!.rpcUrl,
-      deployedContract: DeployedContract(
-        ContractAbi.fromJson(_contractAbi, 'logic_abi'),
-        EthereumAddress.fromHex(_contractAddress),
-      ),
-      topic: _w3mService.session!.topic!,
-      chainId: _w3mService.selectedChain!.namespace,
-      functionName: 'add',
-      transaction: Transaction(
-        from: EthereumAddress.fromHex(_w3mService.session!.address!),
-        value: EtherAmount.fromInt(
-            _etherUnitValue, int.parse(_textEditAmount.text)),
-      ),
+    walletTransactions.addEtherRequest(
+      etherUnitValue: _etherUnitValue,
+      amount: int.parse(_textEditAmount.text),
     );
 
-    final transactionReceipt =
-        await client.getTransactionReceipt(transactionHash);
-
-    print('ADD Transaction: $transactionReceipt');
-
-    _updateContractAmount();
+    final contractAmount = await walletTransactions.deployedContractBalance;
+    setState(() {
+      _currentContractBalance = contractAmount.getValueInUnit(EtherUnit.ether);
+    });
   }
 
   void _retrieveEtherRequest() async {
-    _w3mService.launchConnectedWallet();
-    final String transactionHash = await _w3mService.requestWriteContract(
-      rpcUrl: _w3mService.selectedChain!.rpcUrl,
-      deployedContract: DeployedContract(
-        ContractAbi.fromJson(_contractAbi, 'logic_abi'),
-        EthereumAddress.fromHex(_contractAddress),
-      ),
-      topic: _w3mService.session!.topic!,
-      chainId: _w3mService.selectedChain!.namespace,
-      functionName: 'retrieve',
-      transaction: Transaction(
-        from: EthereumAddress.fromHex(_w3mService.session!.address!),
-      ),
-    );
+    walletTransactions.retrieveEtherRequest();
 
-    final transactionReceipt =
-        await client.getTransactionReceipt(transactionHash);
-
-    print('Retrieve Transaction: $transactionReceipt');
-    _updateContractAmount();
-  }
-
-  void _updateContractAmount() async {
-    final contractAmount =
-        await client.getBalance(EthereumAddress.fromHex(_contractAddress));
-
+    final contractAmount = await walletTransactions.deployedContractBalance;
     setState(() {
-      _currentContractBalance = contractAmount.getInEther;
+      _currentContractBalance = contractAmount.getValueInUnit(EtherUnit.ether);
     });
   }
 
@@ -237,16 +93,16 @@ class _MyHomePageState extends State<MyHomePage> {
                   color: Theme.of(context).colorScheme.primary,
                   size: 48,
                 ),
-                W3MConnectWalletButton(service: _w3mService),
+                W3MConnectWalletButton(service: walletTransactions.w3MService),
               ],
             ),
             const SizedBox(height: 32),
             // connect your wallet buttons
-            W3MNetworkSelectButton(service: _w3mService),
-            W3MAccountButton(service: _w3mService),
+            W3MNetworkSelectButton(service: walletTransactions.w3MService),
+            W3MAccountButton(service: walletTransactions.w3MService),
             const SizedBox(height: 12),
             ElevatedButton.icon(
-              onPressed: _deployContract,
+              onPressed: walletTransactions.deployContract,
               icon: const Icon(Icons.rocket_launch),
               label: const Text('Deploy Contract'),
             ),
@@ -264,7 +120,16 @@ class _MyHomePageState extends State<MyHomePage> {
                             .map(
                               (e) => DropdownMenuItem<EtherUnit>(
                                 value: e,
-                                child: Text(e.name.toUpperCase()),
+                                child: Text(
+                                  e.name.toUpperCase(),
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .labelLarge!
+                                      .copyWith(
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .primary),
+                                ),
                               ),
                             )
                             .toList(),
---
+++ lib/models/wallet_transactions.dart
@@ -0,0 +1,170 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'package:web3modal_flutter/web3modal_flutter.dart';
+
+const projectID = String.fromEnvironment('PROJECT_ID');
+
+const _chainId = '31337'; //hardhat default chainId
+
+final _hardHatChain = W3MChainInfo(
+  chainName: 'Hardhat',
+  namespace: 'eip155:$_chainId',
+  chainId: _chainId,
+  tokenName: 'ETH',
+  rpcUrl: 'http://localhost:8545',
+);
+
+class WalletTransactions {
+  WalletTransactions() {
+    _loadContract();
+    _initWalletConnect();
+  }
+
+  late final W3MService _w3mService;
+  late final String _contractAbi;
+  late final Uint8List _contractData;
+  late final String _contractAddress;
+
+  void _initWalletConnect() async {
+    _w3mService = W3MService(
+      projectId: projectID,
+      //logLevel: LogLevel.debug,
+      metadata: const PairingMetadata(
+        name: 'Web3Modal Flutter Example',
+        description: 'Web3Modal Flutter Example',
+        url: 'https://www.walletconnect.com/',
+        icons: ['https://walletconnect.com/walletconnect-logo.png'],
+        redirect: Redirect(
+          native: 'flutterdapp://',
+          universal: 'https://www.walletconnect.com',
+        ),
+      ),
+    );
+
+    // add hardhat chain to presets
+    W3MChainPresets.chains
+        .putIfAbsent(_hardHatChain.chainId, () => _hardHatChain);
+    await _w3mService.init();
+  }
+
+  // load contract from Remix json
+  void _loadContract() async {
+    final contractCode =
+        await rootBundle.loadString('contracts/artifacts/Logic.json');
+    final abi = jsonDecode(contractCode)['abi'];
+    final bytecode = jsonDecode(contractCode)['data']['bytecode']['object'];
+
+    debugPrint('contract bytecode: $bytecode');
+    debugPrint('contract abi: ${abi.toString()}');
+
+    _contractAbi = jsonEncode(abi);
+    _contractData = hexToBytes(bytecode);
+  }
+
+  // to learn more about rpc methods:
+  // https://ethereum.org/en/developers/docs/apis/json-rpc/
+  void deployContract() async {
+    final client = Web3Client(_w3mService.selectedChain!.rpcUrl, http.Client());
+
+    final transaction = Transaction(
+      // you need to define the from when doing transaction to push contract code
+      from: EthereumAddress.fromHex(_w3mService.session!.address!),
+      data: _contractData,
+    );
+
+    // always launch the wallet before making a Transaction with Web3Modal
+    _w3mService.launchConnectedWallet();
+
+    final String transactionHash = await _w3mService.request(
+      topic: _w3mService.session!.topic!,
+      // we need the namespace format here
+      chainId: _w3mService.selectedChain!.namespace,
+      request: SessionRequestParams(
+        // w3model has string constant to help with request
+        method: MethodsConstants.ethSendTransaction,
+        params: [transaction.toJson()],
+      ),
+    );
+
+    debugPrint('hash: $transactionHash');
+
+    // web3modal doesn't support `eth_getTransactionHash` with _w3mService.request(). However, you have the Web3dart
+    // bundle in Web3Modal so you can use it to get the package like so.
+    // you can list what's available with _w3mService.session!.getApprovedMethods()
+    final transactionReceipt =
+        await client.getTransactionReceipt(transactionHash);
+    final transactionInformation =
+        await client.getTransactionByHash(transactionHash);
+    final networkId = await client.getNetworkId();
+
+    debugPrint('transactionReceipt: $transactionReceipt');
+    debugPrint('transactionInformation: $transactionInformation');
+    debugPrint('network: $networkId');
+
+    _contractAddress = transactionReceipt!.contractAddress!.hex;
+  }
+
+  void addEtherRequest({
+    required EtherUnit etherUnitValue,
+    required int amount,
+  }) async {
+    final client = Web3Client(_w3mService.selectedChain!.rpcUrl, http.Client());
+
+    _w3mService.launchConnectedWallet();
+    final String transactionHash = await _w3mService.requestWriteContract(
+      rpcUrl: _w3mService.selectedChain!.rpcUrl,
+      deployedContract: DeployedContract(
+        ContractAbi.fromJson(_contractAbi, 'logic_abi'),
+        EthereumAddress.fromHex(_contractAddress),
+      ),
+      topic: _w3mService.session!.topic!,
+      chainId: _w3mService.selectedChain!.namespace,
+      functionName: 'add',
+      transaction: Transaction(
+        from: EthereumAddress.fromHex(_w3mService.session!.address!),
+        value: EtherAmount.fromInt(etherUnitValue, amount),
+      ),
+    );
+
+    final transactionReceipt =
+        await client.getTransactionReceipt(transactionHash);
+
+    debugPrint('Add Transaction: $transactionReceipt');
+  }
+
+  void retrieveEtherRequest() async {
+    final client = Web3Client(_w3mService.selectedChain!.rpcUrl, http.Client());
+
+    _w3mService.launchConnectedWallet();
+    final String transactionHash = await _w3mService.requestWriteContract(
+      rpcUrl: _w3mService.selectedChain!.rpcUrl,
+      deployedContract: DeployedContract(
+        ContractAbi.fromJson(_contractAbi, 'logic_abi'),
+        EthereumAddress.fromHex(_contractAddress),
+      ),
+      topic: _w3mService.session!.topic!,
+      chainId: _w3mService.selectedChain!.namespace,
+      functionName: 'retrieve',
+      transaction: Transaction(
+        from: EthereumAddress.fromHex(_w3mService.session!.address!),
+      ),
+    );
+
+    final transactionReceipt =
+        await client.getTransactionReceipt(transactionHash);
+
+    debugPrint('Retrieve Transaction: $transactionReceipt');
+  }
+
+  Future<EtherAmount> get deployedContractBalance async {
+    final client = Web3Client(_w3mService.selectedChain!.rpcUrl, http.Client());
+    return client.getBalance(EthereumAddress.fromHex(_contractAddress));
+  }
+
+  W3MService get w3MService {
+    return _w3mService;
+  }
+}
